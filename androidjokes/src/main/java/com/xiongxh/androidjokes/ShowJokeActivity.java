4919d9b9536e40c8596661039f15e30d522dec8c
xionger <xunhao.xiong@gmail.com>
2017-12-03 14:11:42 -0600

    add android library

---
+++ androidjokes/src/main/java/com/xiongxh/androidjokes/ShowJokeActivity.java
@@ -0,0 +1,21 @@
+package com.xiongxh.androidjokes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ShowJokeActivity extends AppCompatActivity {
+
+    public final static String INTENT_MAIN_JOKE = "INTENT_MAIN_JOKE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_joke);
+
+        String jokeFromMain = getIntent().getStringExtra(INTENT_MAIN_JOKE);
+        TextView jokeView = (TextView) findViewById(R.id.tv_intent_joke);
+
+        jokeView.setText(jokeFromMain);
+    }
+}
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.xiongxh.androidjokes.ShowJokeActivity;
 import com.xiongxh.javajokes.Joker;
 
 
@@ -47,7 +49,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, mJoke, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, mJoke, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, ShowJokeActivity.class);
+        intent.putExtra(ShowJokeActivity.INTENT_MAIN_JOKE, mJoke);
+        startActivity(intent);
     }
 
 
