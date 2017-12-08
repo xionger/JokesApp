8f23f0b6e6f64dbaa6f5f58c03c7b5bf981a5c01
xionger <xunhao.xiong@gmail.com>
2017-12-07 22:38:18 -0600

    add free and paid favors

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/JokeMainFragment.java
@@ -0,0 +1,43 @@
+package com.udacity.gradle.builditbigger.free;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.udacity.gradle.builditbigger.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class JokeMainFragment extends Fragment {
+
+
+    public JokeMainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_joke_main, container, false);
+
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+        mAdView.setVisibility(View.VISIBLE);
+
+        return root;
+    }
+
+}
---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/JokeMainFragment.java
@@ -0,0 +1,31 @@
+package com.udacity.gradle.builditbigger.paid;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.udacity.gradle.builditbigger.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class JokeMainFragment extends Fragment {
+
+
+    public JokeMainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_joke_main, container, false);
+    }
+
+}
